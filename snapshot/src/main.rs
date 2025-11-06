30c131ca7ea3d031305d6ffcff0c559b7003eac9
Samuel Freundel <samuel.freundel@uni-ulm.de>
2025-11-06 14:01:40 +0100

    Implement snahpshot which copies the db and removes if after this

--- snapshot/src/main.rs
+++ snapshot/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::path::Path;
+use shared::{SNAPSHOT_PATH, DB_PATH};
+fn main() -> std::io::Result<()>{
+    if Path::exists(Path::new(&DB_PATH)) {
+        fs::copy(DB_PATH, SNAPSHOT_PATH)?;
+        fs::remove_file(DB_PATH)?
+    }
+    Ok(())
 }
