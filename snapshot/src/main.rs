84e2cadd26f3096feae08cbf08cb544ce828c879
Samuel Freundel <samuel.freundel@uni-ulm.de>
2025-11-06 15:10:46 +0100

    Refactor moved content from main in own method to test better; Tests: add tests to certify that operation works correctly

--- snapshot/src/main.rs
+++ snapshot/src/main.rs
@@ -1,10 +1,16 @@
+mod tests;
+
 use std::fs;
 use std::path::Path;
 use shared::{SNAPSHOT_PATH, DB_PATH};
 fn main() -> std::io::Result<()>{
-    if Path::exists(Path::new(&DB_PATH)) {
-        fs::copy(DB_PATH, SNAPSHOT_PATH)?;
-        fs::remove_file(DB_PATH)?
+    create_snapshot(SNAPSHOT_PATH, DB_PATH)
+}
+
+pub fn create_snapshot(db_path: &str, snapshot_path: &str) -> std::io::Result<()> {
+    if Path::exists(Path::new(&db_path)) {
+        fs::copy(db_path, snapshot_path)?;
+        fs::remove_file(db_path)?
     }
     Ok(())
 }
