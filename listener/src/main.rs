be071dfb97540b72815b0fc36953b68e3a3b6f3e
Samuel Freundel <samuel.freundel@uni-ulm.de>
2025-11-06 13:01:56 +0100

    Restructured Project structure, create projects for future restire and tui; Implementing the should ignore function which gets the environment of the opened proccess to see if it hast the RESTORE_SKIP flag set to 1

--- listener/src/main.rs
+++ listener/src/main.rs
@@ -7,14 +7,11 @@ mod ignored_handler;
 use std::io::{BufRead};
 use crate::event_db::EventDb;
 use crate::event_entry::EventEntry;
-use crate::ignored_handler::IgnoredClasses;
+use crate::ignored_handler::IgnoredHandler;
 use crate::instance_handler::InstanceHandler;
 
-
-
 fn main() -> std::io::Result<()> {
-
-    let ignored_handler = IgnoredClasses::new("ignore.txt");
+    let ignored_handler = IgnoredHandler::new("ignore.txt");
     let mut table: EventDb = EventDb::new("log.txt".to_string());
     let instance = InstanceHandler::new();
 
@@ -22,14 +19,13 @@ fn main() -> std::io::Result<()> {
         match line {
             Ok(line) => {
                 let parts: Vec<String> = EventEntry::split(&line);
-
-                if line.contains("openwindow") && !ignored_handler.should_ignore(&parts[3]) {
+                
+                if line.contains("openwindow") && !ignored_handler.should_ignore(&parts[3], &parts[1]) {
                     table.insert(
                         EventEntry::open_window(parts[1].clone(), parts[2].clone(), parts[3].clone(), parts[4].clone())
                     );
-
                 }
-                if line.contains("closewindow") && !ignored_handler.should_ignore(&parts[0]) {
+                if line.contains("closewindow") {
                     table.remove(&parts[1]).expect("Failed to remove entry");
                 }
             }
