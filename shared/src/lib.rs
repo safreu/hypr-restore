ea1e646c1b31344b71b60bcc1644b4eb814ad79a
Samuel Freundel <samuel.freundel@uni-ulm.de>
2025-11-11 14:48:30 +0100

    Feat: add shared path for the RESTORE paht

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,3 +1,4 @@
 pub const DB_PATH: &str = "./tracker.db";
 pub const IGNORE_PATH: &str = "./classes.ignore";
 pub const SNAPSHOT_PATH: &str = "./tracker.snapshot";
+pub const RESTORE_PATH: &str = "./tracker.restore";
