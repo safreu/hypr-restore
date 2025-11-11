8437c29710fb8350bc0dac6ef5e9da18e9a935a9
Samuel Freundel <samuel.freundel@uni-ulm.de>
2025-11-11 16:14:55 +0100

    Docs: Added Documentation for the path tracker lib

--- path_tracker/src/lib.rs
+++ path_tracker/src/lib.rs
@@ -15,12 +15,16 @@ struct ExecutablesMap {
 }
 
 impl ExecutablesMap {
+    /// Creates a new ExecutablesMap by instantiating the fileHandler and the table
     fn new() -> Self {
         Self {
             file_handler: FileHandler::new(shared::EXECUTABLE_PATH.to_string()),
             table: HashMap::new()
         }
     }
+    /// Initializes the table inside of Executables, by reading the file which contains the paths and writing them into the table,
+    /// # Warning
+    /// **Must** be called **after** new()
     fn init(&mut self) {
         let lines = self.file_handler.read_file().expect("Failed to read file");
         for line in lines {
@@ -29,21 +33,44 @@ impl ExecutablesMap {
         }
     }
 
-    fn insert(&mut self, class: String, path: String) -> io::Result<()> {
-        if self.table.contains_key(&class) && self.table[&class] == path { return Ok(()); }
-        else { self.table.insert(class.clone(), path.clone()); }
+    /// Inserts the class and the path to the table,
+    /// also writes it into the file if the table changes
+    ///
+    /// # Arguments
+    /// * `class` := the class for the application [KEY]
+    /// * `executable_path` := the path to execute the application [VALUE], can be determined with get_executable_path_from_env
+    ///
+    /// # Return
+    /// io::Result based on if the operation was successfully or not
+    fn insert(&mut self, class: String, executable_path: String) -> io::Result<()> {
+        if self.table.contains_key(&class) && self.table[&class] == executable_path { return Ok(()); }
+        else { self.table.insert(class.clone(), executable_path.clone()); }
         self.file_handler.write_complete_hashmap(&self.table)
     }
 
+    /// Gets the Executable Path fromm the environment
+    ///
+    /// # Arguments
+    /// `address` := address Hyprland distributes
+    ///
+    /// # Returns
+    /// the Path to execute the application
     fn get_executable_path_from_env(address: &str) -> Result<String, Error> {
         let pid = utils::get_pid(address);
         utils::get_env_value("_=", &pid)
     }
-    
+
+    /// Gets the executable path in the table
+    ///
+    /// # Arguments
+    /// `class` := the class of the application
+    ///
+    /// # Returns
+    /// The Option of the entry
     fn get_executable_entry(&mut self, class: &str) -> Option<String> {
         if self.table.contains_key(class) {
             return Some(self.table[class].to_string())
-        } 
+        }
         None
     }
 }
