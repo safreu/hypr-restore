b259dfb76577c702b7cd2b66a9c7c73b8381726e
Samuel Freundel <samuel.freundel@uni-ulm.de>
2025-11-11 19:10:11 +0100

    Feat: Add the logic from path_tracker to the listener

--- listener/src/main.rs
+++ listener/src/main.rs
@@ -3,6 +3,7 @@ mod event_db;
 mod ignored_handler;
 
 use std::io::{BufRead};
+use path_tracker::ExecutablesMap;
 use shared::{DB_PATH, IGNORE_PATH};
 use crate::event_db::EventDb;
 use shared::event_entry::EventEntry;
@@ -13,13 +14,20 @@ fn main() -> std::io::Result<()> {
     let ignored_handler = IgnoredHandler::new(IGNORE_PATH);
     let mut table: EventDb = EventDb::new(DB_PATH.to_string());
     let instance = InstanceHandler::new();
+    let mut executables_handler = ExecutablesMap::new(shared::EXECUTABLE_PATH);
+    executables_handler.init();
 
     for line in instance.reader().lines() {
         match line {
             Ok(line) => {
                 let parts = EventEntry::split(&line);
+
                 if line.contains("openwindow") {
                     let event: EventEntry = EventEntry::new(&parts);
+                    match ExecutablesMap::get_executable_path_from_env(&event.address()) {
+                        Ok(executable_path) => executables_handler.insert(event.class().to_string(), executable_path)?,
+                        Err(_) => {executables_handler.insert(event.class().to_string(), event.class().to_string())?;}
+                    }
                     if !ignored_handler.should_ignore(event.class(), event.address()) {
                         table.insert(event);
                     }
