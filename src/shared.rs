ddf1e93019a7d2d66a2e5d4be5aa5f0f5f31d4f7
Samuel Freundel <samuel.freundel@uni-ulm.de>
2025-11-27 12:21:34 +0100

    Fix: Cant use env flags to set information because some apps use one process for all of their windows so they share their environment, so removal of the function

--- src/shared.rs
+++ src/shared.rs
@@ -1,8 +1,6 @@
-use std::{fs, io};
-use std::io::ErrorKind;
+use serde_json::Value;
 use std::path::PathBuf;
 use std::process::Command;
-use serde_json::Value;
 
 pub mod event_entry;
 pub mod file_handler;
@@ -69,17 +67,18 @@ pub fn get_pid(address: &str) -> i64 {
         return 0;
     }
 
-    let pid: i64= 0;
+    let pid: i64 = 0;
     let output = Command::new("hyprctl")
         .args(["clients", "-j"])
         .output()
         .expect("Failed to run hyprctl");
 
-    if !output.status.success() { return 0; }
+    if !output.status.success() {
+        return 0;
+    }
     let stdout = String::from_utf8_lossy(&output.stdout);
 
-    let json: Value = serde_json::from_str(&stdout)
-        .expect("Failed to parse output");
+    let json: Value = serde_json::from_str(&stdout).expect("Failed to parse output");
 
     if let Some(array) = json.as_array() {
         for value in array {
@@ -89,27 +88,4 @@ pub fn get_pid(address: &str) -> i64 {
         }
     }
     pid
-}
-
-/// Searches {pid}/environ for the specified flag and returns its value
-///
-/// # Arguments
-///
-/// * `flag_to_search` := the Flag you want the value for must be the exact Name and must contain = at the end
-/// * `pid` := the pid you want to search for the specified flag
-///
-/// # Returns
-/// The String containing the value or an io Error
-pub fn get_env_value(flag_to_search: &str, pid: &i64) -> Result<String, io::Error> {
-    let path = format!("/proc/{pid}/environ");
-    let content = fs::read_to_string(&path)?;
-
-    for entry in content.split('\0') {
-        if entry.starts_with(flag_to_search) {
-            if let Some((_key, value)) = entry.split_once('=') {
-                return Ok(value.to_string());
-            }
-        }
-    }
-    Err(io::Error::new(ErrorKind::NotFound, "Flag not found"))
 }
